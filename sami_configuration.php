915bd55c9fe27c5e4b96fb340d9aef17107b6fa0
Michael Mayer <michael@schnittstabil.de>
2016-01-30 18:15:26 +0100

    fix unused use statement

--- sami_configuration.php
+++ sami_configuration.php
@@ -1,8 +1,8 @@
 <?php
+
 include 'vendor/autoload.php';
 
 use Sami\Sami;
-use Sami\Version\GitVersionCollection;
 use Symfony\Component\Finder\Finder;
 
 $iterator = Finder::create()
@@ -12,8 +12,8 @@ $iterator = Finder::create()
 ;
 
 return new Sami($iterator, array(
-    'title'                => 'Schnittstabil\Get API',
-    'build_dir'            => __DIR__.'/doc',
-    'cache_dir'            => __DIR__.'/build/cache',
+    'title' => 'Schnittstabil\Get API',
+    'build_dir' => __DIR__.'/doc',
+    'cache_dir' => __DIR__.'/build/cache',
     'default_opened_level' => 2,
 ));
